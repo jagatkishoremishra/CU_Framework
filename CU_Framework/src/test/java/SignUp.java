4b8bf102bbaa50625c78ad943d1013c89d4d7f93
Jagat Kishore Mishra <jagat.kishoremishra@landisgyr.com>
2019-02-02 22:42:17 +0530

    Jagat Signed-off-by: Jagat Kishore Mishra <jagat.kishoremishra@landisgyr.com>

--- CU_Framework/src/test/java/SignUp.java
+++ CU_Framework/src/test/java/SignUp.java
@@ -47,7 +47,7 @@ public class SignUp
 	    // Write code here that turns the phrase above into concrete actions
 	    // For automatic transformation, change DataTable to one of
 	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
-	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)   
 	    throw new PendingException();
 	}
 
